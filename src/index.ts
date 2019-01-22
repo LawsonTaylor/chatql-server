e7523c5c541e1adae10c16362a31fc669a10ad8c
Lawson Taylor <lawson.taylor@protonmail.com>
2019-01-22 11:41:19 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { GraphQLServer  } from 'graphql-yoga';
+import * as cors from 'cors';
 import pubsub from './pubsub';
 import typeDefs from './schema';
 import  resolvers from './resolver';
-
 
 const server = new GraphQLServer(
     {
@@ -10,6 +10,11 @@ const server = new GraphQLServer(
         resolvers, 
         context: { pubsub } 
     });
+
+
+server.express.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 server.start(() => { 
     console.log('Server is running on localhost:4000')
